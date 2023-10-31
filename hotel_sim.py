f942589168442974499141ead1beffa03aeb1f75
samira <samirakorag@gmail.com>
2023-10-31 19:55:57 -0400

    added some docstrings

--- hotel_sim.py
+++ hotel_sim.py
@@ -2,9 +2,13 @@ import sys
 import json
 import argparse
 
-
-class YourClass():
-    def __init__(self):
+"""A hotel booking simulator, that takes in user input and returns
+the  best hotel for their needs. """
+class User_Input():
+    """Takes in user input and sets it to various variables
+    
+    """
+    def __init__(self, name):
         pass
 
 
