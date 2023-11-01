cec0ab40f97e91e457af0ca34db9bb1f80f75191
kteferaa <kasiatefera16@gmail.com>
2023-10-31 21:32:41 -0400

    Updated docstrings

--- hotel_sim.py
+++ hotel_sim.py
@@ -5,14 +5,29 @@ import argparse
 """A hotel booking simulator, that takes in user input and returns
 the  best hotel for their needs. """
 class User_Input():
+   
     """Takes in user input and sets it to various variables
     
     """
     def __init__(self, name):
+         
+        """creates name attribute to store name as argument.
+
+        Args: 
+        name (str): the user's name
+        
+        Side Effects:
+        Creates 'name' attribute.
+    
+        """
         pass
 
 
 def main():
+    """Finds the the hotel that matches the user preferences based on 
+    the user's input using the data from the specificed file.
+    
+    """
     pass
 
 if __name__ == '__main__':
